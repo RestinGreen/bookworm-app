dc0d8cd11cbbd9a1ca28ff0f864a0f66841cc4e2
RestinGreen <majairober@gmail.com>
2025-08-22 19:28:40 +0300

    login backend

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const generateToken = (userId) => {
 
-    jwt.sign(
+    return jwt.sign(
         { userId }, process.env.JWT_SECRET, { expiresIn: "15d" }
     )
 
@@ -65,8 +65,34 @@ router.post("/register", async (req, res) => {
 
 })
 
-router.post("login", async (req, res) => {
-    res.send("login")
+router.post("/login", async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+
+        if (!email || !password) return res.status(400).json({ message: "All fields are required" });
+        const user = await User.findOne({email: email});
+
+        if (!user) return res.status(400).json({message: "Invalid credentials"});
+        const isPasswordCorrect = await user.matchPassword(password);
+        if (!isPasswordCorrect) return res.status(400).json({message: "Invalid credentials"});
+
+        const jwt = generateToken(user._id);
+        res.status(200).json({
+            jwt,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                profileImage: user.profileImage
+            }
+        });
+
+    } catch (error) {
+        console.log("Error in login route", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 
 })
 
