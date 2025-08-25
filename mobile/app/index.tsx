5bd565179f89d2d21c2bec1886da35878eb0c558
RestinGreen <majairober@gmail.com>
2025-08-25 21:36:52 +0300

    frontend project setup

---
+++ mobile/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import {Image} from "expo-image";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen123.</Text>
+    </View>
+  );
+}
